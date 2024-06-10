ec27bc51f2e312293602326c53ac1eda2a225f75
gks6141 <gks6141@hanmail.net>
2024-06-10 14:48:05 +0900

    quiz03 수정 0.1v

--- src/main/java/com/test/lesson03/InsertQuiz03.java
+++ src/main/java/com/test/lesson03/InsertQuiz03.java
@@ -1,5 +1,28 @@
 package com.test.lesson03;
 
-public class InsertQuiz03 {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import com.test.common.MysqlService;
+
+@WebServlet("/lesson03/InsertQuiz03")
+public class InsertQuiz03 extends HttpServlet{
+	
+	@Override
+	public void doPost(HttpServletRequest requset, HttpServletResponse response) {
+		
+		MysqlService ms = MysqlService.getInstance();
+		ms.connect();
+		
+		//
+		String SelectQuery="";
+		
+		
+		ms.disconnect();
+		
+		response.sendRedirect("/lesson03/quiz03.jsp");
+		
+	}
 }
